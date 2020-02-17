69055bd869369f02b2aadbfb61a91a1242141775
Altaf Quadri <altafmquadri@gmail.com>
2020-02-17 13:45:26 -0500

    render vs component for routes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import Dog from './Dog'
 import Contact from './Contact'
 import About from './About'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, NavLink } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <nav className='App-nav'>
+        <NavLink exact activeClassName="active-link" to='/'> Home </NavLink>
+        <NavLink activeClassName="active-link" to='/dog'> Dog </NavLink>
+        <NavLink activeClassName="active-link" to='/contact'> Contact </NavLink>
+      </nav>
       <Switch>
-        <Route exact path='/' component={About} />
-        <Route path='/dog' component={Dog} />
+        <Route path='/dog' render={(routerProps) => <Dog {...routerProps} />} />
         <Route path='/contact' component={Contact} />
+        <Route exact path='/' component={About} />
       </Switch>
     </div>
   );
--- src/Dog.js
+++ src/Dog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Dog = (props) => {
+    console.log(props)
     return (
         <div>
             <h1> Dog!!</h1>
